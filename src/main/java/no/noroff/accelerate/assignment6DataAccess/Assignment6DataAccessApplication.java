d28d2c2f83e4d922c30f5ff691fcd72d407e43de
OleHJoh <johansen.ole.h@gmail.com>
2022-02-23 01:17:46 +0100

    Initial commit

---
+++ src/main/java/no/noroff/accelerate/assignment6DataAccess/Assignment6DataAccessApplication.java
@@ -0,0 +1,13 @@
+package no.noroff.accelerate.assignment6DataAccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment6DataAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment6DataAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/noroff/accelerate/assignment6DataAccess/Assignment6DataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package no.noroff.accelerate.assignment6DataAccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment6DataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
